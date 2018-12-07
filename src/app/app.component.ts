f74297f72f763198dc67aa1d6b31f76284ab292a
Ishikawa <kbc16a01@stu.kawahara.ac.jp>
2018-12-07 13:19:07 +0900

    [add]基本的な紹介追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  title: string;
+  name: string;
+  age: number;
+  job: string;
+  constructor() {}
+  ngOnInit() {
+    this.title = 'My Profile';
+    this.name = '';
+    this.age = null;
+    this.job = '';
+  }
 }
